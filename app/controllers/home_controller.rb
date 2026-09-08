fb3d5f698565e959b1bcc93be569235ab4d3d3ce
Mykola Lyzanets <mykolalyzanets@example.com>
2026-09-08 15:55:26 +0300

    asdf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,30 @@ class HomeController < ApplicationController
     posts = Post.visible.fresh.includes(:category, :source).newest.to_a
     pictured = posts.select(&:photo?)
     @lead = pictured.find(&:main) || pictured.first
-    rest = posts.reject { |post| post.id == @lead&.id }
-    @rail = rest.first(3)
-    @latest = rest.first(5)
-    @business_posts = rest.select { |post| %w[business technology].include?(post.category.url) }.first(4)
-    @most_posts = Post.visible.fresh.includes(:category).popular.limit(4)
+    used = [@lead&.id].compact
+
+    @rail = unused(posts, used).first(2)
+    used.concat(@rail.map(&:id))
+
+    @latest = unused(posts, used).first(5)
+    used.concat(@latest.map(&:id))
+
+    @business_posts = unused(posts, used).select { |post| %w[business technology].include?(post.category.url) }.first(4)
+    used.concat(@business_posts.map(&:id))
+
+    @most_posts = unused(Post.visible.fresh.includes(:category).popular.to_a, used).first(4)
+    used.concat(@most_posts.map(&:id))
+
+    @archive = unused(posts, used).first
   end
 
   def not_found
     render template: 'home/not_found', status: :not_found
+  end
+
+  private
+
+  def unused(posts, used)
+    posts.reject { |post| used.include?(post.id) }
   end
 end
